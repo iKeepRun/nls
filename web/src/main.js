02d77fb4c17611cbc534b75bec861cdbdab1ebc4
zq <zq@czq.auto>
2025-10-15 16:53:55 +0800

    增加首页和登录页面，配置页面路由

--- web/src/main.js
+++ web/src/main.js
@@ -1,20 +1,19 @@
-import { createApp } from 'vue'
-import './style.css'
+import {createApp} from 'vue'
 import App from './App.vue'
-
 
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/reset.css';
-import  * as Icons from '@ant-design/icons-vue';
+import * as Icons from '@ant-design/icons-vue';
+import router from "./router";
 
 const app = createApp(App);
 
-app.use(Antd).mount('#app');
+app.use(Antd).use(router).mount('#app');
 
 // 注册所有图标
-const  icons = Icons;
+const icons = Icons;
 for (const i in icons) {
-  app.component(i, icons[i]);
+    app.component(i, icons[i]);
 }
 
 
---
+++ web/src/router/index.js
@@ -0,0 +1,22 @@
+import {createRouter, createWebHistory} from 'vue-router'
+import Home from "../view/home.vue"
+import Login from "../view/login.vue";
+
+const routes = [ {
+  path: "/home",
+  component: Home,
+  // children: [{
+  //   path: "filetrans",
+  //   component: Filetrans,
+  // }]
+},{
+  path: "/login",
+  component: Login
+}]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
