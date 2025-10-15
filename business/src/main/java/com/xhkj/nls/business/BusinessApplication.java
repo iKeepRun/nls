3ecdcaac6b3235b3cdfefb6eeb24e7739e284dd2
zq <zq@czq.auto>
2025-10-15 09:51:09 +0800

    项目初始化，集成mybatis-plus,输出持久层日志

--- business/src/main/java/com/xhkj/nls/business/BusinessApplication.java
+++ business/src/main/java/com/xhkj/nls/business/BusinessApplication.java
@@ -1,9 +1,11 @@
 package com.xhkj.nls.business;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.xhkj.nls.business.mapper")
 public class BusinessApplication {
 
 	public static void main(String[] args) {
