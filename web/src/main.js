437eeaac8e2045c483ba7bd4eb57ebf3d1b0636a
zq <zq@czq.auto>
2025-10-15 15:34:40 +0800

    引入所有图标，简化开发

--- web/src/main.js
+++ web/src/main.js
@@ -2,4 +2,19 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/reset.css';
+import  * as Icons from '@ant-design/icons-vue';
+
+const app = createApp(App);
+
+app.use(Antd).mount('#app');
+
+// 注册所有图标
+const  icons = Icons;
+for (const i in icons) {
+  app.component(i, icons[i]);
+}
+
+
