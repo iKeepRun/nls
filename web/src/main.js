40155fefa522e154f65ff063bf87711f01146fc3
zq <zq@czq.auto>
2025-10-16 10:10:50 +0800

    添加拦截器

--- web/src/main.js
+++ web/src/main.js
@@ -5,6 +5,7 @@ import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/reset.css';
 import * as Icons from '@ant-design/icons-vue';
 import router from "./router";
+import axios from "axios";
 
 const app = createApp(App);
 
@@ -15,5 +16,25 @@ const icons = Icons;
 for (const i in icons) {
     app.component(i, icons[i]);
 }
+
+
+axios.interceptors.request.use(config => {
+    // config.headers['Authorization'] = localStorage.getItem('token');
+    console.log("requrst inteceptor",config);
+    return config;
+}, error => {
+    return Promise.reject(error);
+})
+
+axios.interceptors.response.use(response => {
+    console.log("response inteceptor",response);
+    return response;
+}, error => {
+    if (error.response.status === 401) {
+        localStorage.removeItem('token');
+        router.push('/login');
+    }
+    return Promise.reject(error);
+});
 
 
